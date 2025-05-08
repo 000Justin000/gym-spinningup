71b6cfb82be869ddfd46a78193e3b10e519ba2ea
Junteng Jia <juntengjia@meta.com>
2025-05-07 18:41:47 -0700

    update

--- main.py
+++ main.py
@@ -2,41 +2,42 @@ import click
 import json
 from utils.nn.module import setup_module
 
-# from torchvision import transforms
+from torchvision import transforms
 
-# import gym
-# from time import sleep
+import gym
+from time import sleep
 
 
 @click.command()
 @click.option("--model_config", type=click.Path(exists=True), required=True)
-def main(config_path: str):
-    with open(config_path, "r") as f:
+def main(model_config: str):
+    with open(model_config, "r") as f:
         model_config = json.load(f)
     model = setup_module(model_config)
     print(model)
 
-    # preprocess = transforms.Compose(
-    #     [
-    #         transforms.ToTensor(),
-    #         transforms.Grayscale(num_output_channels=1),
-    #         transforms.Resize((84, 84)),
-    #     ]
-    # )
-    # print(preprocess)
+    preprocess = transforms.Compose(
+        [
+            transforms.ToTensor(),                       # [H, W, C] -> [C, H, W]
+            transforms.Grayscale(num_output_channels=1), # [C, H, W] -> [1, H, W]   
+            transforms.Resize((84, 84)),                 # [1, H, W] -> [1, 84, 84]
+        ]
+    )
+    print(preprocess)
 
-    # env = gym.make("MsPacmanNoFrameskip-v4", render_mode="human")
-    # env.reset()
-    # env.render()
+    env = gym.make("MsPacmanNoFrameskip-v4", render_mode="human")
+    obs, info = env.reset()
 
-    # for _ in range(1000):
-    #     action = env.action_space.sample()
-    #     ob_next, reward, terminated, truncated, info = env.step(action)
-    #     env.render()
+    q = model(preprocess(obs).unsequeeze(0))
 
-    #     if terminated or truncated:
-    #         env.reset()
-    #     sleep(0.01)
+    for _ in range(1000):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        env.render()
+
+        if terminated or truncated:
+            obs, info = env.reset()
+        sleep(0.01)
 
 
 if __name__ == "__main__":
