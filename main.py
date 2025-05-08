91761d989636e57c3efd1385e8c5858de81f4a44
Junteng Jia <juntengjia@meta.com>
2025-05-07 17:40:51 -0700

    update

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from utils.nn.module import setup_module
 
 
 @click.command()
-@click.option("--config_path", type=click.Path(exists=True), required=True)
+@click.option("--model_config", type=click.Path(exists=True), required=True)
 def main(config_path: str):
     with open(config_path, "r") as f:
-        config = json.load(f)
-    model = setup_module(config["model"])
+        model_config = json.load(f)
+    model = setup_module(model_config)
     print(model)
 
 
--- utils/nn/layer.py
+++ utils/nn/layer.py
@@ -1,6 +1,7 @@
 from .module import BaseModule, register_module
 from typing import Dict
 from torch import Tensor
+import torch
 import torch.nn as nn
 
 
@@ -30,6 +31,60 @@ class LinearLayer(BaseLayer):
         self.module = nn.Linear(in_dim, out_dim, bias=bias)
         self.in_key = in_key
         self.out_key = out_key
+
+    def forward(self, dict: Dict[str, Tensor]):
+        x = dict[self.in_key]
+        dict[self.out_key] = self.module(x)
+        return dict
+
+
+@register_module
+class ReLULayer(BaseLayer):
+    def __init__(
+        self,
+        in_key: str,
+        out_key: str,
+    ):
+        super().__init__()
+        self.in_key = in_key
+        self.out_key = out_key
+
+    def forward(self, dict: Dict[str, Tensor]):
+        x = dict[self.in_key]
+        dict[self.out_key] = torch.relu(x)
+        return dict
+
+
+@register_module
+class Conv2dLayer(BaseLayer):
+    def __init__(
+        self,
+        in_key: str,
+        out_key: str,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int = 1,
+        padding: int = 0,
+        dilation: int = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = "zeros",
+    ):
+        super().__init__()
+        self.in_key = in_key
+        self.out_key = out_key
+        self.module = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias,
+            padding_mode=padding_mode,
+        )
 
     def forward(self, dict: Dict[str, Tensor]):
         x = dict[self.in_key]
