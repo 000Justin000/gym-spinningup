36a65b79aeaf2c720b98d8c7fae2bff8d409e3bf
Junteng Jia <juntengjia@meta.com>
2025-05-08 20:11:06 -0700

    update

--- main.py
+++ main.py
@@ -22,8 +22,47 @@ import time
 import numpy as np
 
 
+SLEEP_TIME = 0.0
+NUM_EPISODES = 100000
+MAX_NUM_STEPS = 1000
+EPSILON = 0.2
+GAMMA = 0.999
+BATCH_SIZE = 32
+REPLAY_BUFFER_SIZE = 100000
+STACK_SIZE = 1
+LR = 0.0001
+DEVICE = "mps"
+
 log_dir = os.path.join("runs", "dqn_" + time.strftime("%Y%m%d-%H%M%S"))
 writer = SummaryWriter(log_dir)
+
+
+class StateManager:
+    def __init__(self, stack_size):
+        self.stack_size = stack_size
+        self.frames = deque(maxlen=stack_size)
+        self.preprocess = transforms.Compose(
+            [
+                transforms.ToTensor(),  # [H, W, C] -> [C, H, W]
+                transforms.Grayscale(num_output_channels=1),  # [C, H, W] -> [1, H, W]
+                transforms.Resize((84, 84)),  # [1, H, W] -> [1, 84, 84]
+                transforms.Lambda(
+                    lambda x: x.unsqueeze(0)
+                ),  # [1, 84, 84] -> [1, 1, 84, 84]
+                transforms.Lambda(
+                    lambda x: x.to(DEVICE)
+                ),  # [1, 1, 84, 84] -> [1, 1, 84, 84]
+            ]
+        )
+
+    def push(self, observation):
+        frame = self.preprocess(observation)
+        while len(self.frames) < self.stack_size:
+            self.frames.append(frame)
+        self.frames.append(frame)
+
+    def get(self):
+        return torch.cat(list(self.frames), dim=-1)
 
 
 class ReplayBuffer:
@@ -37,29 +76,15 @@ class ReplayBuffer:
     def sample(self, batch_size=None):
         if batch_size is None:
             batch_size = self.default_batch_size
-        transitions = random.sample(self.buffer, batch_size)
+        transitions = random.choices(self.buffer, k=batch_size)
         states, actions, rewards, next_states, dones = zip(*transitions)
         return (
             torch.cat(states, dim=0),
-            torch.tensor(actions).reshape(-1, 1),
-            torch.tensor(rewards).reshape(-1, 1),
+            torch.tensor(actions).reshape(-1, 1).to(DEVICE),
+            torch.tensor(rewards).reshape(-1, 1).to(DEVICE),
             torch.cat(next_states, dim=0),
-            torch.tensor(dones).reshape(-1, 1),
+            torch.tensor(dones).reshape(-1, 1).to(DEVICE),
         )
-
-
-class StackedFrames:
-    def __init__(self, stack_size):
-        self.stack_size = stack_size
-        self.frames = deque(maxlen=stack_size)
-
-    def push(self, frame):
-        while len(self.frames) < self.stack_size:
-            self.frames.append(frame)
-        self.frames.append(frame)
-
-    def get(self):
-        return torch.cat(list(self.frames), dim=-1)
 
 
 def soft_update(target_model, source_model, tau=0.05):
@@ -79,66 +104,47 @@ def main(model_config: str):
     policy_model = setup_module(model_config).to("mps")
     target_model = copy.deepcopy(policy_model).eval()
 
-    preprocess = transforms.Compose(
-        [
-            transforms.ToTensor(),  # [H, W, C] -> [C, H, W]
-            transforms.Grayscale(num_output_channels=1),  # [C, H, W] -> [1, H, W]
-            transforms.Resize((84, 84)),  # [1, H, W] -> [1, 84, 84]
-            transforms.Lambda(
-                lambda x: x.unsqueeze(0)
-            ),  # [1, 84, 84] -> [1, 1, 84, 84]
-            transforms.Lambda(
-                lambda x: x.to("mps")
-            ),  # [1, 1, 84, 84] -> [1, 1, 84, 84]
-        ]
-    )
-    print(preprocess)
-
     # env = gym.make("MsPacman-v4", render_mode="human")
     env = gym.make("MsPacman-v4")
-
-    RENDER_FREQ = 0
-    SLEEP_TIME = 0.0
-    NUM_EPISODES = 100000
-    MAX_NUM_STEPS = 1000
-    EPSILON = 0.2
-    GAMMA = 0.999
-    BATCH_SIZE = 32
-    REPLAY_BUFFER_SIZE = 100000
-    STACK_SIZE = 1
-    LR = 0.001
 
     def select_action(state, policy_model):
         if random.random() < EPSILON:
             return env.action_space.sample()
         else:
-            return policy_model({"x": preprocess(state)})["x"].argmax(dim=-1).item()
+            return policy_model({"x": state})["x"].argmax(dim=-1).item()
 
     optimizer = torch.optim.Adam(policy_model.parameters(), lr=LR)
     buffer = ReplayBuffer(REPLAY_BUFFER_SIZE, BATCH_SIZE)
     for episode in range(NUM_EPISODES):
-        frames = StackedFrames(STACK_SIZE)
+        state_manager = StateManager(STACK_SIZE)
         obs, info = env.reset()
-        frames.push(preprocess(obs))
+        state_manager.push(obs)
         list_reward = []
         list_q_est = []
         list_loss = []
         for step in range(MAX_NUM_STEPS):
-            state = frames.get()
+            state = state_manager.get()
             action = select_action(state, policy_model)
             obs, reward, terminated, truncated, info = env.step(action)
-            frames.push(preprocess(obs))
-            next_state = frames.get()
+            state_manager.push(obs)
+            next_state = state_manager.get()
             buffer.push(state, action, reward, next_state, terminated)
 
             # training
-            batch_state, batch_action, batch_reward, batch_next_state, batch_terminated = buffer.sample()
+            (
+                batch_state,
+                batch_action,
+                batch_reward,
+                batch_next_state,
+                batch_terminated,
+            ) = buffer.sample()
             q = policy_model({"x": batch_state})["x"]
-            next_q = target_model({"x": batch_next_state})["x"]
+            with torch.no_grad():
+                next_q = target_model({"x": batch_next_state})["x"]
 
             # compute loss
             q_est = torch.gather(q, dim=-1, index=batch_action)
-            q_tgt = batch_reward + GAMMA * next_q.max(dim=-1).values * (1 - batch_terminated)
+            q_tgt = batch_reward + GAMMA * next_q.max(dim=-1, keepdim=True).values * ~batch_terminated
             loss = F.mse_loss(q_est, q_tgt)
 
             # update NN
